e2893815d2739dce4de69637299942b51a2b3646
hydru1 <hydru65535@gmail.com>
2023-02-01 22:59:27 +0900

    combination

--- Python3/math/combination/mod-combination.py
+++ Python3/math/combination/mod-combination.py
@@ -1,16 +1,16 @@
-mod = 10**9+7
-N = int(input())
+class combination():
+    def __init__(self,n,mod):
+    
+        self.mod=mod
+        self.fact = [1,1] # x!
+        self.factinv = [1,1] # (x!)^(-1)
+        inv = [0,1]
+        for i in range(2,n+1):
+            self.fact.append((self.fact[-1]*i)%mod)
+            inv.append((-inv[mod%i]*(mod//i))%mod)
+            self.factinv.append((self.factinv[-1]*inv[-1])%mod)
 
-fact = [1,1] # n!
-factinv = [1,1] # (n!)^(-1)
-inv = [0,1]#
-for i in range(2,N+1):
-    fact.append((fact[-1]*i)%mod )
-    inv.append((-inv[mod%i]*(mod//i))%mod)
-    factinv.append((factinv[-1]*inv[-1])%mod)
-
-def cmb(n,r,mod):
-    r = min(r,n-r)
-    return fact[n]*factinv[r]*factinv[n-r]%mod
-
+    def cmb(self,n,r):
+        r=min(r,n-r)
+        return self.fact[n]*self.factinv[r]*self.factinv[n-r]%self.mod
 
