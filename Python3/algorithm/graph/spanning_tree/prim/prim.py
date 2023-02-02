827a9c2f1035c469366fb41e527a1376dc137204
hydru1 <hydru65535@gmail.com>
2023-02-02 18:48:37 +0900

    プリム法を追加

---
+++ Python3/algorithm/graph/spanning_tree/prim/prim.py
@@ -0,0 +1,36 @@
+from heapq import heapify,heappush,heappop
+
+def Prim(G):
+    n=len(G)
+    visit = [0 for _ in range(n)]
+
+    visit_cnt = 0
+    visit[0] = 1
+    visit_cnt += 1
+
+    q = []
+    heapify(q)
+
+    for j, c in G[0]:
+        heappush(q,(c,j,0))
+
+    edge=[]
+
+    while visit_cnt < n:
+        c,i,j = heappop(q)
+
+        if visit[i]:
+            continue
+
+        visit[i] = 1
+        visit_cnt += 1
+
+        edge.append((i,j)) 
+
+        for j, c in G[i]:
+            if visit[j]:
+                continue
+
+            heappush(q, (c,j,i))
+
+    return edge
